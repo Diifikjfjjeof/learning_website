8ba71ee3b7f06fdd064fe2192cbbb1d0568c455f
Lilian <lilian901125@gmail.com>
2022-07-27 22:41:32 +0800

    Initial Commit

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app.secret_key = 'd;lf;ldgkflg;dlf;'
 ckeditor = CKEditor(app)
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] =  os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] =  os.environ.get("DATABASE_URL",  "sqlite:///addbase.db")
 
 # -----------------------------------------------set up form here----------------
 class addform(FlaskForm):
